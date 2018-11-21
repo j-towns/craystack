6456cfa14fcf5e9da7dc07511e1f079a019da5ed
Jamie Townsend <jamiehntownsend@gmail.com>
2018-11-21 14:14:55 +0000

    Add bb_ans

---
+++ craystack/bb_ans.py
@@ -0,0 +1,141 @@
+import numpy as np
+from scipy.stats import norm
+import craystack.craystack as cs
+
+
+def BBANS(prior, likelihood, posterior):
+    """
+    This codec is for data modelled with a latent variable model as described
+    in the paper 'Practical Lossless Compression with Latent Variable Models'
+    currently under review for ICLR '19.
+
+       latent        observed
+      variable         data
+
+        ( z ) ------> ( x )
+
+    This assumes data x is modelled via a model which includes a latent
+    variable. The model has a prior p(z), likelihood p(x | z) and (possibly
+    approximate) posterior q(z | x). See the paper for more details.
+    """
+    prior_append, prior_pop = prior
+
+    def append(message, data):
+        _, posterior_pop = posterior(data)
+        message, latent = posterior_pop(message)
+        likelihood_append, _ = likelihood(latent)
+        message = likelihood_append(message, data)
+        message = prior_append(message, latent)
+        return message
+
+    def pop(message):
+        message, latent = prior_pop(message)
+        _, likelihood_pop = likelihood(latent)
+        message, data = likelihood_pop(message)
+        posterior_append, _ = posterior(data)
+        message = posterior_append(message, latent)
+        return message, data
+    return append, pop
+
+def VAE(gen_net, rec_net, obs_codec, prior_prec=8, latent_prec=12):
+    """
+    This codec uses the BB-ANS algorithm to code data which is distributed
+    according to a variational auto-encoder (VAE) model. It is assumed that the
+    VAE uses an isotropic Gaussian prior and diagonal Gaussian for its
+    posterior.
+    """
+    z_view = lambda head: head[0]
+    x_view = lambda head: head[1]
+
+    prior = cs.substack(cs.Uniform(prior_prec), z_view)
+
+    def likelihood(latent_idxs):
+        z = std_gaussian_centres(prior_prec)[latent_idxs]
+        return cs.substack(obs_codec(gen_net(z)), x_view)
+
+    def posterior(data):
+        post_mean, post_stdd = rec_net(data)
+        return cs.substack(_DiagGaussianLatent(
+            post_mean, post_stdd, prior_prec, latent_prec), z_view)
+    return BBANS(prior, likelihood, posterior)
+
+def _nearest_int(arr):
+    # This will break when vectorized
+    return np.uint64(np.ceil(arr - 0.5))
+
+std_gaussian_bucket_cache = {}  # Stores bucket endpoints
+std_gaussian_centres_cache = {}  # Stores bucket centres
+
+def std_gaussian_buckets(precision):
+    """
+    Return the endpoints of buckets partioning the domain of the prior. Each
+    bucket has mass 1 / (1 << precision) under the prior.
+    """
+    if precision in std_gaussian_bucket_cache:
+        return std_gaussian_bucket_cache[precision]
+    else:
+        buckets = norm.ppf(np.linspace(0, 1, (1 << precision) + 1))
+        std_gaussian_bucket_cache[precision] = buckets
+        return buckets
+
+def std_gaussian_centres(precision):
+    """
+    Return the centres of mass of buckets partioning the domain of the prior.
+    Each bucket has mass 1 / (1 << precision) under the prior.
+    """
+    if precision in std_gaussian_centres_cache:
+        return std_gaussian_centres_cache[precision]
+    else:
+        centres = np.float32(
+            norm.ppf((np.arange(1 << precision) + 0.5) / (1 << precision)))
+        std_gaussian_centres_cache[precision] = centres
+        return centres
+
+def _gaussian_latent_cdf(mean, stdd, prior_prec, post_prec):
+    def cdf(idx):
+        x = std_gaussian_buckets(prior_prec)[idx]
+        return _nearest_int(norm.cdf(x, mean, stdd) * (1 << post_prec))
+    return cdf
+
+def _gaussian_latent_ppf(mean, stdd, prior_prec, post_prec):
+    def ppf(cf):
+        x = norm.ppf((cf + 0.5) / (1 << post_prec), mean, stdd)
+        # Binary search is faster than using the actual gaussian cdf for the
+        # precisions we typically use, however the cdf is O(1) whereas search
+        # is O(precision), so for high precision cdf will be faster.
+        return np.uint64(np.digitize(x, std_gaussian_buckets(prior_prec)) - 1)
+    return ppf
+
+def _DiagGaussianLatent(mean, stdd, prior_prec, post_prec):
+    enc_statfun = cs.cdf_to_enc_statfun(
+        _gaussian_latent_cdf(mean, stdd, prior_prec, post_prec))
+    dec_statfun = _gaussian_latent_ppf(mean, stdd, prior_prec, post_prec)
+    return cs.NonUniform(enc_statfun, dec_statfun, post_prec)
+
+
+# VAE observation codecs
+def _ensure_nonzero_freq_bernoulli(p, precision):
+    p[p == 0] += 1
+    p[p == (1 << precision)] -=1
+    return p
+
+def _bernoulli_cdf(p, precision, safe=True):
+    def cdf(s):
+        ret = np.zeros(np.shape(s), "uint64")
+        onemp = _nearest_int((1 - p[s==1]) * (1 << precision))
+        onemp = (_ensure_nonzero_freq_bernoulli(onemp, precision) if safe
+                 else onemp)
+        ret[s == 1] += onemp
+        ret[s == 2] = 1 << precision
+        return ret
+    return cdf
+
+def _bernoulli_ppf(p, precision, safe=True):
+    onemp = _nearest_int((1 - p) * (1 << precision))
+    onemp = _ensure_nonzero_freq_bernoulli(onemp, precision) if safe else onemp
+    return lambda cf: np.uint64((cf + 0.5) > onemp)
+
+def Bernoulli(p, prec):
+    enc_statfun = cs.cdf_to_enc_statfun(_bernoulli_cdf(p, prec))
+    dec_statfun = _bernoulli_ppf(p, prec)
+    return cs.NonUniform(enc_statfun, dec_statfun, prec)
