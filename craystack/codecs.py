656b82e15800734f983eda4ac92e121e72a48d6f
tom-bird <tombird1992@gmail.com>
2019-05-01 16:06:26 +0100

    add random stack fn

--- craystack/codecs.py
+++ craystack/codecs.py
@@ -115,6 +115,11 @@ def reshape_head(message, shape):
     head, tail = reshape_head_1d(message, size=np.prod(shape))
     return np.reshape(head, shape), tail
 
+def random_stack(flat_len, shape, rng=np.random):
+    """Generate a random vrans stack"""
+    arr = rng.randint(1 << 32, size=flat_len, dtype='uint32')
+    return unflatten_benford(arr, shape)
+
 def _ensure_nonzero_freq_bernoulli(p, precision):
     p[p == 0] += 1
     p[p == (1 << precision)] -=1
