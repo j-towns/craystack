9d42c2a9916c4ed903d60c300489786c0c02bc48
Jamie Townsend <jamiehntownsend@gmail.com>
2021-08-11 17:08:39 +0200

    Correct version number, add @dsevero to authors.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='craystack',
-    version='0.0',
+    version='0.2',
     description='Compression tools for machine learning researchers',
-    author='Jamie Townsend and Thomas Bird and Julius Kunze',
+    author='Jamie Townsend and Thomas Bird and Julius Kunze and Daniel Severo',
     author_email='jamiehntownsend@gmail.com',
     packages=['craystack'],
     install_requires=['numpy', 'scipy', 'autograd'],
