Add fix for pushing and popping with finer precision in rans (#16)
* Add fix for pushing and popping with finer precision in rans

* Update rans_test.py

* Update rans_test.py

* Add issue link to regression test

---------

Co-authored-by: Jamie Townsend <jamiehntownsend@gmail.com>

craystack/rans.py
@@ -90,25 +90,48 @@ def unflatten(arr, shape):
 def message_equal(message1, message2):
     return np.all(flatten(message1) == flatten(message2))
 
-
 def push_with_finer_prec(ans_state, starts, freqs, precisions):
     head, tail = ans_state
+    # head in [2 ^ 32, 2 ^ 64)
     starts, freqs, precisions = map(atleast_1d, (starts, freqs, precisions))
     idxs = head >= ((rans_l // precisions) << 32) * freqs
     if np.any(idxs):
         tail = stack_extend(tail, np.uint32(head[idxs]))
         head = np.copy(head)  # Ensure no side-effects
         head[idxs] >>= 32
+    # head in [((rans_l // precisions)) * freqs), ((rans_l // precisions)) * freqs) << 32)
 
     # calculate next state s' = 2^r * (s // p) + (s % p) + c
     head_div_freqs, head_mod_freqs = np.divmod(head, freqs)
-    return head_div_freqs*precisions + head_mod_freqs + starts, tail
+    head = head_div_freqs * precisions + head_mod_freqs + starts
 
+    # head in [precisions * (2 ^ 32 // precisions), precisions * ((2 ^ 32 // precisions) << 32))
+    # check which entries need renormalizing
+    idxs = head < rans_l
+
+    # how many 32*n bits do we need from the tail?
+    n = np.sum(idxs)
+    if n > 0:
+        # new_head = 32*n bits from the tail
+        # tail = previous tail, with 32*n less bits
+        tail, new_head = stack_slice(tail, n)
+
+        # update LSBs of head, where needed
+        head[idxs] = (head[idxs] << 32) | new_head
+    # head in [2 ^ 32, 2 ^ 64)
+    return head, tail
 
 def pop_with_finer_prec(ans_state, precisions):
     precisions = atleast_1d(precisions)
     head_, tail_ = ans_state
+    # head_ in [2 ^ 32, 2 ^ 64)
+    idxs = head_ >= precisions * ((rans_l // precisions) << 32)
+    if np.any(idxs):
+        tail_ = stack_extend(tail_, np.uint32(head_[idxs]))
+        head_ = np.copy(head_)  # Ensure no side-effects
+        head_[idxs] >>= 32
 
+    # head in [precisions * (2 ^ 32 // precisions), precisions * ((2 ^ 32 // precisions) << 32))
     # s' mod 2^r
     cfs = head_ % precisions
 